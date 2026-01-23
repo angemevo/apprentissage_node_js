40cc4c2fb56b9d11d940bb3f044fd2b862385e50
angemevo <mevoange903@gmail.com>
2026-01-23 16:21:37 +0000

    app mobile

---
+++ api_flutter_application/note_app/lib/config/constants.dart
@@ -0,0 +1,8 @@
+class AppiConstants {
+  static const String baseUrl = 'http://10.0.2.2:3000/api';
+
+  // Endpoints
+  static const String register = ('/auth/register');
+  static const String login = ('/auth/login');
+  static const String register = ('/auth/register');
+}
